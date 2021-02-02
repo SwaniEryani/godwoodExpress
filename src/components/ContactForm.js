0f2430ba1cf784b1c3bca1b64825ce2e2ed740e8
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2021-02-02 13:19:58 -0500

    add the contact us

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import'./ContactForm.css';
-//https://blog.mailtrap.io/react-contact-form/
-export default function ContactForm() {
 
+export default function ContactForm() {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
   return (
-    <form id="contact-form" method="POST">
+    <form id="contact-form" onSubmit={handleSubmit}>
     <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input type="text" className="form-control" placeholder="Your name.." />
+        <input type="text" className="form-control" placeholder="Your name.." id="name" required />
     </div>
     <div className="form-group">
         <label htmlFor="inputEmail1">Email address</label>
-        <input type="email" className="form-control" aria-describedby="emailHelp"  placeholder="Your Email.."/>
+        <input type="email" className="form-control" aria-describedby="emailHelp"  placeholder="Your Email.." id="email" required />
     </div>
     <div className="form-group">
         <label htmlFor="message">Message</label>
-        <textarea className="form-control" rows="5" placeholder="Your message.." ></textarea>
+        <textarea className="form-control" rows="5" placeholder="Your message.." id="message" required ></textarea>
     </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
+    <button type="submit" className="btn btn-primary">{status}</button>
 </form>
   )
 }
